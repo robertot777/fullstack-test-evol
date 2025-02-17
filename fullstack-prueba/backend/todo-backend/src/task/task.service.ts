0120db51eb5847d1a912c407d4b0807a588c14c5
robertot777 <33211784+robertot777@users.noreply.github.com>
2025-02-16 22:30:01 -0300

    node
    node

--- fullstack-prueba/backend/todo-backend/src/task/task.service.ts
+++ fullstack-prueba/backend/todo-backend/src/task/task.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TaskService {
+  remove(id: number): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  update(id: number, task: Task): Promise<Task> {
+    throw new Error('Method not implemented.');
+  }
+  create(task: Task): Promise<import("./task.entity").Task> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<import("./task.entity").Task[]> {
+    throw new Error('Method not implemented.');
+  }
   // Add your service methods here
 }
